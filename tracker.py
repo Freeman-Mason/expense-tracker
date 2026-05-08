5f2d436ad4c993d1faf05e6cfdc795b01557184b
Freeman <223882259@edu.vut.ac.za>
2026-05-09 01:38:18 +0200

    Add ExpenseTracker class

--- tracker.py
+++ tracker.py
@@ -12,3 +12,29 @@ class Expense:
     def __repr__(self):
 
         return f"Expense({self.amount}, {self.category}, {self.date}, {self.description})"
+
+class ExpenseTracker:
+    def __init__(self):
+        self.expenses = []
+
+
+    def add_expense(self, expense):
+        if not isinstance(expense, Expense):
+            raise TypeError("Must be an Expense object")
+        self.expenses.append(expense)
+
+    def get_all_expenses(self):
+        return self.expenses
+
+    def get_total(self):
+        total = 0
+        for expense in self.expenses:
+            total += expense.amount
+        return total
+
+    def get_by_category(self, category):
+        result = []
+        for expense in self.expenses:
+            if expense.category == category:
+                result.append(expense)
+        return result
